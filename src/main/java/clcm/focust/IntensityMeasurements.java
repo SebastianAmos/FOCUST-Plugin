bebb62963b66459d94859d9de21d6b98873990f1
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-04-17 17:47:28 +0800

    Cleaned up commented code

--- src/main/java/clcm/focust/IntensityMeasurements.java
+++ src/main/java/clcm/focust/IntensityMeasurements.java
@@ -1,6 +1,5 @@
 package clcm.focust;
 
-import ij.IJ;
 import ij.ImagePlus;
 import ij.macro.Variable;
 import ij.measure.ResultsTable;
@@ -16,44 +15,32 @@ public class IntensityMeasurements {
 		
 		// Instance the IntensityMeasures class from MorpholibJ
 		final IntensityMeasures im = new IntensityMeasures(input, label);
-		ResultsTable table = new ResultsTable();  // move this into Process function otherwise it might not reset between images in batch.
+		ResultsTable table = new ResultsTable();  
 		ResultsTable rtVolume = new ResultsTable();
 		ResultsTable rtMean = new ResultsTable();
-		IJ.log("input image name is: " + input.getTitle());
-		IJ.log("label image name is: " + label.getTitle());
 		
 		rtVolume = im.getVolume();
 		rtMean = im.getMean();
 
-		
 		// calculate Integrated Density
 		double[] vol = rtVolume.getColumnAsDoubles(0);
-		//IJ.log("vol array is: " + vol);
 		double[] intMean = rtMean.getColumnAsDoubles(0);
-		//IJ.log("mean array is: " + intMean);
 		Variable[] intDen = new Variable[vol.length];
-		
 		
 		// test to see if writing to table vs array fixes overwrite problem
 		for (int i = 0; i < vol.length; i++) {
 			intDen[i] = new Variable(vol[i] * intMean[i]);
-			//double volVar = vol[i];
-			//double meanVar = intMean[i];
-			//double intDenVar = volVar * meanVar; 
-			//rtVolume.addValue("IntDen", intDenVar);
 		}
-		
 		Variable[] labArray = rtMean.getColumnAsVariables("Label");
 		Variable[] volumeArray = rtVolume.getColumnAsVariables("Volume");
 		Variable[] meanIntensityArray = rtMean.getColumnAsVariables("Mean");
-		//Variable[] IntDenArray = rtVolume.getColumnAsVariables("IntDen");
 		
+
 		
 		table.setColumn("Label", labArray);
 		table.setColumn("Mean_Intensity", meanIntensityArray);
 		table.setColumn("Volume", volumeArray);
 		table.setColumn("IntDen", intDen);
-		//table.setColumn("IntDen", IntDenArray);
 		return table;
 	}
 }
