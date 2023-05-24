701053cc7aa8ad7089ac8ee7dc9ef361d6d91c2c
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-05-24 14:09:38 +0800

    Methods to scan labels and reassign if duplicated

--- src/main/java/clcm/focust/LabelEditor.java
+++ src/main/java/clcm/focust/LabelEditor.java
@@ -26,11 +26,92 @@ public class LabelEditor {
 	*/ 
 	
 	
+	public static ImagePlus findAndRename(ImagePlus matched, ImagePlus original) {
+		
+		Map<Integer, Integer> labelMaps = new HashMap<>();
+		
+		// iterate over slices of matched image to map the labels
+		// for every z slice, at every x and y pixel position, compare the label values.
+		for (int z = 1; z <= matched.getStackSize(); z++) {
+			ImageProcessor ipMatched = matched.getStack().getProcessor(z);
+			ImageProcessor ipOriginal = original.getStack().getProcessor(z);
+			
+			for (int y = 0; y < ipMatched.getHeight(); y++) {
+				for (int x = 0; x < ipMatched.getWidth(); x++) {
+					int lblMatched = (int) ipMatched.getPixelValue(x, y);
+					int lblOriginal = (int) ipOriginal.getPixelValue(x, y);
+					
+					// where a pixel label exists, and the map contains that value, reassign the value.
+					if (lblMatched > 0 ) {
+						if (labelMaps.containsKey(lblMatched)) {
+							
+							// shared value found, append an index to individualize the label 
+							int newIndex = labelMaps.get(lblMatched) + 1 ;
+							labelMaps.put(lblMatched, newIndex);
+							ipOriginal.putPixelValue(x, y, Double.parseDouble( lblOriginal + "." + newIndex));
+							
+						} else {
+							// new label found, add to the map
+							labelMaps.put(lblMatched, 0);
+							ipOriginal.putPixelValue(x, y, lblOriginal);
+						}
+					}
+				}
+			}
+		}
+		
+		return original;
+	} // end of findAndRename method
+	
+	
+	public static ImagePlus manageDuplicates(ImagePlus matched, ImagePlus original) {
+		
+		// Create a map to store label values from the original image. If a new value is encountered, initialise from 0 so the first repeat can index from 1. 
+		Map<Integer, Integer> labelMaps = new HashMap<>();
+		
+		// Iterate over the original image stack to map the labels, reassigning values in the matched image where required.
+		// For each z slice, at every x, y pixel position compare label values
+		for (int z = 1; z <= original.getStackSize(); z++) {
+			
+			ImageProcessor ipMatched = matched.getStack().getProcessor(z);
+			ImageProcessor ipOriginal = original.getStack().getProcessor(z);
+			
+			for (int y = 0; y < ipOriginal.getHeight(); y++) {
+				for (int x = 0; x < ipOriginal.getWidth(); x++) {
+					
+					int lblMatched = (int) ipMatched.getPixelValue(x, y);
+					int lblOriginal = (int) ipOriginal.getPixelValue(x, y);
+					
+					// If there is a label (i.e. not background which = 0), and it exists in the map, reassign the pixel value with the same label + index.
+					if (lblOriginal > 0) {
+						if (labelMaps.containsKey(lblOriginal)) {
+							
+							// Shared label exists, append an index to induvidualise the instance
+							int newIndex = labelMaps.get(lblOriginal) + 1;
+						
+							labelMaps.put(lblOriginal, newIndex);
+							ipMatched.putPixelValue(x, y, Double.parseDouble(lblMatched + "." + newIndex));
+						
+						} else {
+							// Initialise new label keys with an index value of 0, so if found again, indexing for repeats can start at 1. 
+							labelMaps.put(lblOriginal, 0);
+							ipMatched.putPixelValue(x, y, lblMatched);
+						}
+					}
+				}
+			}
+		}
+		return matched;
+	} // end of manageDuplicates method
+	
+	
+	
+	
+	
+	
+
 	public static ImagePlus relativeRename(ImagePlus matched, ImagePlus original) {
-		
 		Map<Integer, Integer> labelMapping = new HashMap<>();
-		
-		
 		// iterate over pixels of matched image
 		for (int z = 1; z <= matched.getStackSize(); z++) {
 			ImageProcessor ip = matched.getStack().getProcessor(z);
@@ -43,7 +124,6 @@ public class LabelEditor {
 				}
 			}	
 		}
-		
 		int i = 0;
 		for (int z = 1; z<= original.getStackSize(); z++) {
 			ImageProcessor ip = original.getStack().getProcessor(z);
@@ -58,9 +138,9 @@ public class LabelEditor {
 				}
 			}
 		}
-		
+		renamedImage = original;
 		return renamedImage;
-	}
+	} // end of relativeRename method
 	
 	
 	
@@ -68,13 +148,13 @@ public class LabelEditor {
 	
 	
 	
-	
+	// clij2 approach to find labels in matched that have the same 
 	public static ImagePlus gpuRenameLabels(ImagePlus matched, ImagePlus original) {
 		ResultsTable rtMatched = analyze3D.process(matched);
 		ResultsTable rtOriginal = analyze3D.process(original);
 		
-		double[] lblsMatched = rtMatched.getColumnAsDoubles(-1);
-		double[] lblsOriginal = rtOriginal.getColumnAsDoubles(-1);
+		double[] lblsMatched = rtMatched.getColumnAsDoubles(0);
+		double[] lblsOriginal = rtOriginal.getColumnAsDoubles(0);
 		
 		CLIJ2 clij2 = CLIJ2.getInstance();
 		clij2.clear();
@@ -115,37 +195,29 @@ public class LabelEditor {
 		
 		// unique labels to objects
 		for(int i = 0; i < lblsMatched.length; i++) {
-			ClearCLBuffer label = bufMatched;
+			double label = lblsMatched[i];
+			double uniqueLabel = label + maxLabelMatched *(i+1);
+			clij2.addImageAndScalar(uniqueLabels, uniqueLabels, uniqueLabel);
+			clij2.greaterConstant(uniqueLabels, uniqueLabels, label);
 		}
 		
+		double maxLabelOriginal = clij2.maximumOfAllPixels(bufOriginal);
+		
+		ClearCLBuffer updateLabels = clij2.create(bufOriginal);
+		clij2.set(updateLabels, 0);
+		
+		clij2.addImageAndScalar(bufOriginal, updateLabels, maxLabelMatched + 1);
+		clij2.greaterConstant(updateLabels, updateLabels, maxLabelMatched + 1);
+		clij2.mask(updateLabels, updateLabels, uniqueLabels);
+		clij2.addImageAndScalar(updateLabels, updateLabels, maxLabelOriginal);
 		
 		
+		renamedImage = clij2.pull(updateLabels);
 		
 		
 		return renamedImage;
 	}
 	
-	
-	
-	
-	public static ImagePlus manageDuplicates(ImagePlus matched, ImagePlus original) {
-		
-		// generate results tables so labels can be accessed
-		ResultsTable rtMatched = analyze3D.process(matched);
-		ResultsTable rtOriginal = analyze3D.process(original);
-		
-		// grab label columns as double arrays
-		double[] lblsMatched = rtMatched.getColumnAsDoubles(-1);
-		double[] lblsOriginal = rtOriginal.getColumnAsDoubles(-1);
-		
-		//int[] labelMapping = createLabelMapping(lblsMatched, lblsOriginal);
-		
-		
-		
-		
-		
-		return renamedImage;
-	}// end of manageDuplicates method
 	
 	
 	
