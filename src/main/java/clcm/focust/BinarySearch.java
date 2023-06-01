18480df710c33d26854dc5238a4c3c3125e44d72
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-06-01 16:45:37 +0800

    Implements binary search algo to compute spheroid core

--- src/main/java/clcm/focust/BinarySearch.java
+++ src/main/java/clcm/focust/BinarySearch.java
@@ -1,17 +1,16 @@
 package clcm.focust;
 
+import ij.IJ;
 import ij.ImagePlus;
 import inra.ijpb.plugins.AnalyzeRegions3D;
 import process3d.Erode_;
 import ij.measure.ResultsTable;
-import ij.process.BinaryProcessor;
-import ij.process.ByteProcessor;
-import ij.process.ImageProcessor;
 
 public class BinarySearch {
 
 	public static AnalyzeRegions3D analyze3D = new AnalyzeRegions3D(); // from MorphoLibJ
-	public static Erode_ erode3D = new Erode_(); // from VIB_ FIJI lib
+	public static Erode_ erode3D = new Erode_(); // from VIB_ FIJI library
+	
 	
 	
 	/**
@@ -27,14 +26,15 @@ public class BinarySearch {
 		ImagePlus img = LabelEditor.makeBinary(imp);
 		
 		// compute max number of iterations possible on current image.
-		int maxIterations = maxIterations(img.getWidth(), img.getHeight(), img.getStackSize());
-		
+		int maxIterations = maxIterations(img.getWidth(), img.getHeight());
 		System.out.println("Maximum number of iterations is: " + maxIterations); // testing!
 		
 		// original volume of current image.
 		int originalVoxNum = calculateVoxNum(img.duplicate());
 		
 		System.out.println("Original Voxel Count is: " + originalVoxNum); // testing!
+		int targetVoxNum = (int) (0.5 * originalVoxNum);
+		System.out.println("Target Voxel Count is: " + targetVoxNum); // testing!
 		
 		// compute optimal number of erosion iterations.
 		int optimalIterationNum = binarySearchIterationNumber(img.duplicate(), maxIterations, originalVoxNum);
@@ -47,6 +47,7 @@ public class BinarySearch {
 	}
 
 
+	
 	/**
 	 * Compute the max number of iterations possible given the dimensions of the image.
 	 * This sets the upper limit for a binary search method.
@@ -59,14 +60,16 @@ public class BinarySearch {
 	 * @return The max possible iteration number.
 	 */
 	
-	private static int maxIterations(int w, int h, int z) {
+	private static int maxIterations(int w, int h) {
 		int smallerDimension = Math.min(w, h);
-		return smallerDimension / 2 * z;
+		return smallerDimension / 2;
 	}
+	
 	
 	
 	/**
 	 * Computes the total number of voxels in an image.
+	 * If voxel count is 0, return. 
 	 * 
 	 * @param imp
 	 * 			An ImagePlus to be measured.
@@ -75,10 +78,16 @@ public class BinarySearch {
 	
 	private static int calculateVoxNum(ImagePlus imp) {
 		ResultsTable rt = analyze3D.process(imp);
-		int voxNum = (int) rt.getValue("VoxelCount", 0);
-		return voxNum;
+		int voxNum = 0;
+		try {
+			voxNum = (int) rt.getValue("VoxelCount", 0);
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
 		
+		return voxNum;
 	}
+	
 	
 	
 	/**
@@ -99,45 +108,31 @@ public class BinarySearch {
 		int lower = 0;
 		int higher = maxIterations;
 		int targetIterationNumber = -1;
+	
+		
+		int targetVoxNum = (int) (0.5 * originalVoxNum);
+		IJ.log("Total volume = " + originalVoxNum);
+		IJ.log("Target volume = " + targetVoxNum);
 		
 		while (lower <= higher) {
 			int middle = (lower + higher) / 2;
-			ImagePlus erodedImg = erodeStack(imp, middle);
+			ImagePlus erodedImg = erodeStack(imp.duplicate(), middle);
 			int voxNum = calculateVoxNum(erodedImg);
 			if (voxNum <= 0.5 * originalVoxNum) {
 				targetIterationNumber = middle;
-				lower = middle + 1;
-			} else {
 				higher = middle - 1;
+				IJ.log("Current iteration count: " + targetIterationNumber);
+				IJ.log("Vox count too low ( "+voxNum+" ), setting iteration count lower: " + lower +" - " + higher);
+				
+			} else {
+				lower = middle + 1;
+				IJ.log("Vox count too high ( "+voxNum+" ), setting iteration count higher: " + lower +" - " + higher);
 			}
 		}
+		System.out.println("Final iteration number: " + targetIterationNumber);
 		return targetIterationNumber;
 	}
 	
-	
-	/**
-	 * Erodes a binary image i number of times. 
-	 * 
-	 * @param ip 
-	 * 			ImageProcessor input image to be eroded by i iterations.
-	 * @param iterations
-	 * 			The number of times ip should be eroded. 
-	 * 
-	 * @return An ImageProcessor containing the eroded mask.
-	 */
-	
-	private ImageProcessor erodeImage(ImageProcessor ip, int iterations) {
-		ByteProcessor byteProcessor = ip.convertToByteProcessor();
-		BinaryProcessor bp = new BinaryProcessor(byteProcessor);
-		
-		for (int i = 0; i < iterations; i++) {
-			bp.erode();
-		}
-		
-		// may need to set a threshold bp.setThreshold(upper, lower) implementing 200, 255, for example.
-		return bp.createMask();
-		
-	}
 	
 	
 	/**
