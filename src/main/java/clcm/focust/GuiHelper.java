d04b71a0f94e884ceae54e1d47ebcb7239eb6eda
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-06-07 15:00:20 +0800

    button group handling

---
+++ src/main/java/clcm/focust/GuiHelper.java
@@ -0,0 +1,27 @@
+package clcm.focust;
+
+import java.util.Enumeration;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+
+public class GuiHelper {
+
+	/**
+	 * This method iterates through a button collection and returns the text of the selected radiobutton.
+	 * @param btnGroup
+	 * @return String of the selected radiobutton.
+	 */
+	public static String getSelectedButton(ButtonGroup btnGroup) {
+		for (Enumeration<AbstractButton> btns = btnGroup.getElements(); btns.hasMoreElements();) {
+			AbstractButton btn = btns.nextElement();
+			if (btn.isSelected()) {
+				return btn.getText();
+			}
+			
+		}
+		return null;
+	}
+	
+	
+}
