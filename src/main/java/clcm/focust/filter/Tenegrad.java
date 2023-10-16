905011b5a1048cab0c24dbff04a49be2e78539fb
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-10-16 09:56:34 +0800

    adding filters

---
+++ src/main/java/clcm/focust/filter/Tenegrad.java
@@ -0,0 +1,13 @@
+package clcm.focust.filter;
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+
+public class Tenegrad implements Filter{
+
+	@Override
+	public ClearCLBuffer apply(ClearCLBuffer input, Vector3D v1, Vector3D v2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
