c8106846d383ca3cb823f1ee287a6a846cd2a94a
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-10-18 16:46:55 +0800

    WIP methods

--- src/main/java/clcm/focust/ManageDuplicates.java
+++ src/main/java/clcm/focust/ManageDuplicates.java
@@ -18,83 +18,81 @@ public class ManageDuplicates {
 	
 	
 	/**
-	 * converts original label value to 1 then imposes the values from the "labels" ImagePlus by multiplication.
+	 * converts original label value to 1 then imposes the values from the "labels"
+	 * ImagePlus by multiplication.
 	 * 
-	 * @param labels Imp with labels to impose on the orignal image. i.e. secondary objects.
-	 * @param original Imp with labels that need to adopt the labelling from the "labels" imp. i.e. primary objects.
+	 * @param labels   Imp with labels to impose on the orignal image. i.e.
+	 *                 secondary objects.
+	 * @param original Imp with labels that need to adopt the labelling from the
+	 *                 "labels" imp. i.e. primary objects.
 	 * @return
 	 */
 	public ImagePlus imposeLabels(ImagePlus labels, ImagePlus original) {
-		
+
 		ImagePlus binary = LabelEditor.makeBinary(original);
 		IJ.run(binary, "Divide...", "value=" + binary.getDisplayRangeMax() + " stack");
 		ImagePlus matched = ImageCalculator.run(labels, binary, "Multiply create stack");
-		
+
 		return matched;
 	}
-	
-
 
 	/**
-	 * Compare the labels of two images and store in a map.
-	 * keys = original labels and values = relabelled labels (potentially duplicates)
+	 * Compare the labels of two images and store in a map. keys = original labels
+	 * and values = relabelled labels (potentially duplicates)
+	 * 
 	 * @param original
 	 * @param relabelled
-	 * @return a map with original labels 
+	 * @return a map with original labels
 	 */
-	public Map<Double, Double> matchLabels(ImagePlus original, ImagePlus relabelled){
-		
+	public Map<Double, Double> matchLabels(ImagePlus original, ImagePlus relabelled) {
+
 		final IntensityMeasures im = new IntensityMeasures(original, relabelled);
-		
+
 		Map<Double, Double> labelComparisons = new HashMap<>();
-		
+
 		ResultsTable labels = im.getMax();
-		
+
 		for (int i = 0; i < labels.size(); i++) {
-			
+
 			labelComparisons.put(labels.getValue("Label", i), labels.getValue("Max", i));
 		}
-		
-		
-		
+
 		return labelComparisons;
-		
+
 	}
 
 	
-
 	/**
-	 * for each repeated value encountered, append 0.1. 
+	 * for each repeated value encountered, append 0.1.
+	 * 
 	 * @param map
 	 */
 	public Map<Double, Double> mapDuplicates(Map<Double, Double> map) {
-		
+
 		Map<Double, Integer> labelTracker = new HashMap<>();
 		Map<Double, Double> results = new HashMap<>();
-		
+
 		for (Double key : map.keySet()) {
-			
+
 			Double val = map.get(key);
 			int counter = labelTracker.getOrDefault(val, 0);
 			labelTracker.put(val, counter + 1);
-			
+
 			if (counter > 0) {
 				val += counter * 0.1;
 			}
-			
+
 			// update the counter and results map
-			
+
 			results.put(key, val);
-			
+
 		}
 		System.out.println("Counter Map: " + labelTracker);
-		
+
 		return results;
-		
+
 	}
 
-	
-	
 	/**
 	 * Assign labels from a map where a key is discovered.
 	 * 
@@ -117,7 +115,6 @@ public class ManageDuplicates {
 		}
 		return imp;
 	}
-
 	
 	
 }
