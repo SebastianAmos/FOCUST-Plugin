01b00c8b76d7c80266ff800563bf0dc45b0976d3
Sebastian Amos <113169074+SebastianAmos@users.noreply.github.com>
2023-11-23 16:16:42 +0800

    Update Mode.java

--- src/main/java/clcm/focust/mode/Mode.java
+++ src/main/java/clcm/focust/mode/Mode.java
@@ -1,10 +1,8 @@
 package clcm.focust.mode;
 
-import java.util.ArrayList;
-
 import clcm.focust.data.object.SegmentedChannels;
 import clcm.focust.parameters.ParameterCollection;
-import ij.ImagePlus;
+
 
 public interface Mode {
 	
