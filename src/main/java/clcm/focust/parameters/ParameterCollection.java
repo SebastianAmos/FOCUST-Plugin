d60ec4c0945b1defbabc28cdde3e92ecd5b9c208
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2024-01-22 21:14:35 +0800

    Adds initial attempt at serialisation and de-serialisation of ParameterCollection with associated test. Updates pom.xml to include Gson.

--- src/main/java/clcm/focust/parameters/ParameterCollection.java
+++ src/main/java/clcm/focust/parameters/ParameterCollection.java
@@ -1,13 +1,21 @@
 package clcm.focust.parameters;
+
 import clcm.focust.KillBorderTypes;
 import clcm.focust.data.DataObject;
 import clcm.focust.mode.ModeType;
-import lombok.Data;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.Builder;
+import lombok.Data;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 @Data
 @Builder
-public class ParameterCollection implements DataObject{
+public class ParameterCollection implements DataObject {
     final String inputDir;
     final String outputDir;
     final ModeType mode;
@@ -39,4 +47,14 @@ public class ParameterCollection implements DataObject{
     final boolean tertiaryDisplayOriginal;
     final boolean tertiaryOverlay;
 
+    public static void saveParameterCollection(ParameterCollection parameterCollection, String filename) throws IOException {
+        Writer writer = new FileWriter(parameterCollection.outputDir + filename);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        gson.toJson(parameterCollection, writer);
+    }
+
+    public static ParameterCollection loadParameterCollection(String inputDir, String filename) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.fromJson(new FileReader(inputDir + filename), ParameterCollection.class);
+    }
 }
---
+++ src/test/java/clcm/focust/parameters/ParameterCollectionTest.java
@@ -0,0 +1,35 @@
+package clcm.focust.parameters;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ParameterCollectionTest {
+
+
+    @Test
+    void saveParameterCollection() {
+        ParameterCollection params = ParameterCollection.builder().
+                outputDir("src/test/resources/").build();
+        try {
+            ParameterCollection.saveParameterCollection(params, "parameterCollectionWriteTest.json");
+        }
+        catch (IOException e){
+            fail("IO Exception encountered");
+        }
+    }
+
+    @Test
+    void loadParameterCollection() {
+        String filename = "parameterCollectionReadTest.json";
+        try{
+            ParameterCollection params = ParameterCollection.loadParameterCollection("src/test/resources/", filename);
+            assertEquals("src/test/resources/", params.outputDir);
+        } catch (IOException e){
+            fail("IO Exception encountered");
+        }
+    }
+}
